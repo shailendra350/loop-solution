380336289270e6f033fb01db0893ecf840fc56be
shailendra bhimawad <shailendrabhimawad1501@gmail.com>
2021-02-15 16:55:44 +0530

    loops

---
+++ array.js
@@ -0,0 +1,28 @@
+// var arr = [10,20,30,40,50,60,70,80,90,100]
+ 
+//  console.log(arr.length)
+
+// for(var i=9;i>=0;i--){
+// 	console.log(arr[i])
+// }
+
+
+// var name = "welcome"
+//  var reversestr =""
+
+//  for(var i=name.length-1;i>=0;i--){
+//  	reversestr +=name[i]
+//  }
+
+//  console.log(reversestr)
+
+
+ var a =[10,50]
+
+ function test(b){
+ 	 b[0]=20
+ 	console.log(b)
+ }
+
+test(a)
+console.log(a)
--- loopproblem.js
+++ loopproblem.js
@@ -85,22 +85,24 @@
    // for(var i=1; i<=10; i++) {
    // 	   table =i*Textdata
    // 	   console.log("%d * %d =%d \n", Textdata, i ,table )
+
    // }
 
 
    // 7
    // program to display the multipliaction table vertically from 1 to n
 
-     
-     //  Textdata=9
-     //  i=0
-     // while( i<10 ){
-     // 	i++;
-     // 	  table =i*Textdata
-     // 	   console.log("%d * %d =%d \n",Textdata , i ,table )
-     	  
-     	
-     // }
+    //  var n = 10
+
+    // for(var i=1; i<=10; i++){
+    // var tblStr = ""
+    // for(var j = 1; j<=n; j++){
+    //     tblStr += j+"x"+i+" = "+i*j+" "
+    // }
+    // console.log(tblStr)
+    // }
+ 
+
 
 
 
@@ -127,6 +129,7 @@
     //   console.log( "The Sum of odd Natural Number upto 10 terms-", sum)
 
 
+    
 
 
 
@@ -168,6 +171,56 @@
 
     
 
+       //18
+       //program to find the sum of the series [ 1-X^2/2!+X^4/4!- .........].
+         
+
+
+//        function fact(n){
+
+//     if(n==0){
+//         return 1
+//     }
+
+//     var f = 1;
+//     for(var i=1; i<=n; i++){
+//         f *= i
+//     }
+//     return f
+// }
+
+// function pow(a, b){
+//     var p = 1
+//     for(var i=1; i<=b; i++){
+//         p *= a
+//     }
+//     return p
+// }
+
+// var numOfTerms = 5
+// var x = 2
+
+// var count = 1
+// var i = 0
+// var res = 0
+// while(count<=numOfTerms){
+//     if(i%2 == 0){
+//         var calc = pow(x, i)/fact(i)
+//         if(count%2==0){
+//             res -= calc
+//         }else{
+//             res += calc
+//         }
+//         count ++
+//     }
+//     i++
+// }
+
+// console.log(res) 
+
+
+
+
 
 
     // 19
@@ -189,40 +242,391 @@
  // console.log( "Sum of Series-", sum)          
  
 
+//21
+//program to display the sum of the series [ 9 + 99 + 999 + 9999 ...].
 
-    // 21
-  
-  //   var num=2
-  //   var n=10
-  //   var count=0
-  //   var sum=0
+// var a=10
+// var n=5
+// sum =0
 
-  //   while(count<=num){
-  //   	if(n-1){
-  //   		console.log(n-1)                                         // remaining
-          
-  //         n++
-  //         n*10
-  //         sum+sum+(n-1)
-          
-  //   	}
-    	
-    	 
-  //   }
-  // console.log( "Sum of Series-", sum)          
+// function pow(a, b){
+//     var p = 1
+//     for(var i=1; i<=b; i++){
+//         p *= a
+//     }
+//     return p
+// }
 
 
-  //23
 
-  // var x=3
-  // var power=x*x
-  // var number=5
-  // var count=1
-  // var num=1
+// for(var i=1 ; i<=n ; i++){
+// 	   var s=pow(a,i)-1
+//      sum +=s
+       
 
-  //      while(count<=number){
-  //      	if(num+x){
-  //      		console.log(num+x)
-  //      	}
-  //      	break
-  //      }
+// 	console.log(s)
+// }
+// console.log(sum)
+
+
+
+
+//25
+//program to display the n terms of square natural number and their sum.
+
+//  term=5
+//  sum=0
+
+//  for(var i=1;i<=term;i++){
+//  	s=i*i
+//  	sum +=s
+//  	console.log(s)
+// }
+// console.log("the sum is",sum)
+
+
+
+//26
+//program to find the sum of the series 1 +11 + 111 + 1111 + .. n terms.
+
+
+// var a=10
+// var n=5
+// sum =0
+
+// function pow(a, b){
+//     var p = 1
+//     for(var i=1; i<=b; i++){
+//         p *= a
+//     }
+//     return p
+// }
+
+
+
+// for(var i=1 ; i<=n ; i++){
+// 	   var s=(pow(a,i)-1)/9
+//      sum +=s
+       
+
+// 	console.log(s)
+// }
+// console.log(sum)
+
+
+
+
+//27
+//program to check whether a given number is a perfect number or not.
+
+
+// var num=28
+// sum=0
+// result=0
+// for(var i=1 ; i<=num-1; i++){
+// 	if(num%i==0){
+		 
+//  console.log(i)
+//  sum +=i
+// }
+// 	}
+//  console.log( "the sum is", sum)
+
+// if(sum==num){
+// 	console.log("this number is perfect number")
+// }else{
+// 	console.log("this number is not perfect number")
+// }
+
+
+
+//28
+//
+
+var num=50
+sum=0
+result=0
+
+if(num<=50){
+      
+   for(var i=1 ; i<=num-1 ; i++){
+           for(var j=1 ; j<=num-1 ; j++){
+           	  if(num%i==0)
+           	           {
+           	           	console.log(i)
+           	           	sum +=i
+           	  	  if(sum==num){
+           	  	  	console.log(sum)
+           	  	  }
+           	  	  
+           	  }
+           	  
+           }
+           j++
+           
+   }
+   i++
+   
+
+// 	for(var i=1 ; i<=num-1 ; i++){
+// 			if(num%i==0){
+		 
+//         console.log(i)
+//           sum +=i
+//            }
+         
+// 	}
+// 	console.log("the sum is ",sum)
+
+//    if(sum==num){
+// 	console.log("this number is perfect number")
+// }else{
+// 	console.log("this number is not perfect number")
+// }
+
+
+}
+else{
+	console.log("this is invalid")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
